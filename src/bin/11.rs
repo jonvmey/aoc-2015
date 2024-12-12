2de4224385e2192e680bb998ea13552c9409f811
Jonathan VanderMey <jonathan@vandermey.ca>
2024-12-11 21:46:59 -0500

    Add missing test case

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -123,6 +123,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some("ghjbbcdd".to_string()));
     }
 }
